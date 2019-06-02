629b56c2fdba830436652836c87ca85724270332
MattGThomas <matthewgthomas0616@gmail.com>
2019-06-02 18:15:15 -0500

    added action button, number button, and display components

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Button.css';
+
+const NumberButton = props => {
+    return (
+        <div className = 'buttonsContainer'>
+        <div className = 'buttons'>
+            <button className={props.buttonStyle}>{props.text}</button>
+        </div>
+        </div>
+    )
+}
+
+export default NumberButton;
--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,41 @@
 import React from 'react';
 import './Display.css';
+
+import NumberButton from '../ButtonComponents/NumberButton';
+import ActionButton from '../ButtonComponents/ActionButton';
+
+const CalculatorDisplay = props => {
+    return (
+        <div className='display'>
+        <div className='answerBox'>0</div>
+        <div className='sectionOne'>
+            <ActionButton text='clear' />
+            <NumberButton text='/' />
+        </div>
+        <div className='sectionTwo'>
+            <NumberButton text='7' />
+            <NumberButton text='8' />
+            <NumberButton text='9' />
+            <NumberButton text='x' />
+        </div>
+        <div className='sectionThree'>
+            <NumberButton text='4' />
+            <NumberButton text='5' />
+            <NumberButton text='6' />
+            <NumberButton text='-' />
+        </div>
+        <div className='sectionFour'>
+            <NumberButton text='1' />
+            <NumberButton text='2' />
+            <NumberButton text='3' />
+            <NumberButton text='+' />
+        </div>
+        <div className='sectionFive'>
+            <ActionButton text='0' />
+            <NumberButton text='=' />
+        </div>
+        </div>
+    )
+};
+
+export default CalculatorDisplay;
